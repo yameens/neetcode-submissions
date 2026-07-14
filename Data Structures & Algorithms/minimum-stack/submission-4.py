a94e9d278215f59b2e676cfb56a103fcc5b88d7d
Yameen Sekandari <91277571+yameens@users.noreply.github.com>
2026-07-13 20:37:47 -0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        del self.stack[-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        minimum = float("inf")
+
+        for s in self.stack: 
+            if minimum > s: 
+                minimum = s
+
+        return minimum
